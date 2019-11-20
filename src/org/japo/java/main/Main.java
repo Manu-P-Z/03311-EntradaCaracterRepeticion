b55e85c83562584418576994ca48ec69a92ae393
Manu-P-Z <55140316+Manu-P-Z@users.noreply.github.com>
2019-11-20 20:58:40 +0100

    :tada: Commit Inicial 03311-EntradaCaracterRepeticion

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,54 @@
+/* 
+ * Copyright 2019 Manu Portolés Zagalá.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Manu Portolés Zagalá
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static final Scanner SCN
+            = new Scanner(System.in, "Windows-1252")
+                    .useLocale(Locale.ENGLISH).useDelimiter("\\s+");
+
+    public static void main(String[] args) {
+
+        char caracter;
+        // boolean valido;
+
+        System.out.print("Introduzca un caracter ...: ");
+        // do {
+
+        //   try {
+        caracter = SCN.nextLine().charAt(0);
+        //     valido = true;
+
+        //  } catch (Exception e) {
+        //     System.out.print("Introduzca un caracter ...: ");
+        //    valido = false;
+        // } finally {
+        //   }
+        // } while (valido == false);
+        System.out.printf("%s %c%n", "El valor elegido es", caracter);
+    }
+}
